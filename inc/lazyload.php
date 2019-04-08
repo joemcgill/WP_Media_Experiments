f15058aaaa8e00afb7508062fa692710028d1937
Joe McGill <joemcgill@gmail.com>
2019-04-08 11:36:40 -0500

    Add lazy loading to dynamic images.
    This adds a filter to the 'wp_get_attachment_image_attributes' hook, which adds a `loading` attribute to images and sets the attribute to `loading`.

--- inc/lazyload.php
+++ inc/lazyload.php
@@ -16,6 +16,7 @@ function bootstrap() {
 	add_filter( 'wp_media_experiments/image_content', __NAMESPACE__ . '\\add_lazy_loading_attr' );
 	add_filter( 'wp_media_experiments/gallery_content', __NAMESPACE__ . '\\add_lazy_loading_attr' );
 	add_filter( 'wp_media_experiments/media_text_content', __NAMESPACE__ . '\\add_lazy_loading_attr' );
+	add_filter( 'wp_get_attachment_image_attributes', __NAMESPACE__ . '\\add_loading_to_image_attr' );
 }
 
 /**
@@ -26,4 +27,16 @@ function bootstrap() {
  */
 function add_lazy_loading_attr( $content ) {
 	return str_replace( '<img', '<img loading="lazy"', $content );
+}
+
+/**
+ * Add lazy loading to dynamically generated images.
+ *
+ * @param array $attr Attributes for image markup.
+ * @return array Array of image attributes with lazy loading added.
+ */
+function add_loading_to_image_attr( $attr ) {
+	$attr[ 'loading' ] = 'lazy';
+
+	return $attr;
 }
